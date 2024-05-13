7c90b3890a47c695125f626d30845b79594eb10f
TravisAshcraft <132078620+Travis-Ashcraft@users.noreply.github.com>
2024-05-13 13:31:30 -0500

    sadfa

--- src/sections/About.js
+++ src/sections/About.js
@@ -5,7 +5,7 @@ const About = () => {
     return (
         <section className="about">
             <h2>About Us</h2>
-            <p>TSTC Playhub is tww est designed test to merge education with entertainment, providing effective learning solutions through gamification.</p>
+            <p>TSTC Playhub is tww est designed ewetest to merge education with entertainment, providing effective learning solutions through gamification.</p>
         </section>
     );
 }
